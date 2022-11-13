f862a29815a6c0233514c8a10c4a6461db2f3678
sofiagalkina <gal18024@byui.edu>
2022-11-12 21:50:43 -0700

    pic added and json changed

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,8 +1,14 @@
+const requestURL = 'https://sofiagalkina.github.io/wdd230/chamber/data/data.json';
+
+
+
+
 let cardSelector = document.querySelector("#cardselect");
 let listSelector = document.querySelector("#listselect");
 let cardview = document.querySelector("#cardview");
 let listview = document.querySelector("#listview");
 
+/*
 cardSelector.addEventListener('click', ()=>(
     cardview.style.display='grid';
     listview.style.display="none";
@@ -10,40 +16,25 @@ cardSelector.addEventListener('click', ()=>(
     listSelector.style.opacity=0.5;
 
 ));
-
-
-
-
-
-
-
-
-
-
-
+*/
 
 
 function displayCards(card){
     let cardview = document.querySelector("#cardview");
     let cardelt = document.createElement("div");
-    card.innerHTML = `
-    <div>
-    <img src="${card.imageURL}">
-    <p>${card.street} ${card.citystatezip}</p>
+    cardelt.innerHTML = `<img src="${card.imageURL}">
+    <p>${card.address} ${card.citystatezip}</p>
     <p>${card.phone}</p>
-    <p><a href=${"somesite.com">https://somesite.com</a></p>
-</div>
-`
+    <p><a href="${card.websiteURL}">${card.websiteURL}</a></p>`;
+    cardview.appendChild(cardelt);
 }
 
-
-function displayBusinessList(biz){
+/*function displayBusinessList(biz){
     let card = document.createElement("tr");
     card.innerHTML
 }
+*/
 
-
-const requestURL = "./data/data.json";
 
 
 fetch(requestURL)
@@ -53,7 +44,7 @@ fetch(requestURL)
   .then(function (jsonObject) {
     console.table(jsonObject); 
     
-    const bizlist = jsonObject["prophets"]
+    const bizlist = jsonObject["businesses"]
     bizlist.forEach(displayCards);
     bizlist.forEach(displayList);
   });

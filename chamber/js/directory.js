c0055cc77989236c54f3af984baaade3c32e3520
sofiagalkina <gal18024@byui.edu>
2022-11-17 01:05:01 -0700

    buttons added, data changed

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,22 +1,38 @@
-const requestURL = './data/data.json';
-
-
-
 
 let cardSelector = document.querySelector("#cardselect");
 let listSelector = document.querySelector("#listselect");
 let cardview = document.querySelector("#cardview");
 let listview = document.querySelector("#listview");
 
-/*
-cardSelector.addEventListener('click', ()=>(
-    cardview.style.display='grid';
-    listview.style.display="none";
-    cardSelector.style.opacity=1.0;
-    listSelector.style.opacity=0.5;
 
-));
-*/
+cardSelector.addEventListener('click', ()=>{
+
+  cardview.style.display='grid';
+  listview.style.display='none';
+  cardSelector.style.opacity=1.0;
+  listSelector.style.opacity=0.5;
+});
+
+listSelector.addEventListener('click', ()=>{
+  cardview.style.display='none';
+  listview.style.display="block";
+  cardSelector.style.opacity=0.5;
+  listSelector.style.opacity=1.0;
+});
+
+const requestURL = './data/data.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject); 
+    
+    const bizlist = jsonObject["businesses"]
+    bizlist.forEach(displayCards);
+    bizlist.forEach(displayBusinessList);
+  });
 
 
 function displayCards(card){
@@ -29,22 +45,13 @@ function displayCards(card){
     cardview.appendChild(cardelt);
 }
 
-/*function displayBusinessList(biz){
+function displayBusinessList(biz){
     let card = document.createElement("tr");
-    card.innerHTML
+    card.innerHTML=`<td>${biz.name}</td>
+    <td>${biz.street}<br>${biz.cityandzip}</td>
+    <td>${biz.phone}</td>
+    <td><a href="${biz.websiteURL}">${biz.websiteURL}</a></td>`;
+
+    document.querySelector("#listview table").appendChild(card);
 }
-*/
 
-
-
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject); 
-    
-    const bizlist = jsonObject["businesses"]
-    bizlist.forEach(displayCards);
-    bizlist.forEach(displayList);
-  });

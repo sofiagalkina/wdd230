148304a366390aa4e16e3ea781c651884914a0b0
sofiagalkina <gal18024@byui.edu>
2022-11-29 15:56:32 -0700

    everything's done!!

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -6,8 +6,8 @@
 
 
 /*       FOOTER   */
-const today = new Date();
-document.querySelector("#year").textContent = today.getFullYear();
+const todayy = new Date();
+document.querySelector("#year").textContent = todayy.getFullYear();
 
 document.getElementById("modify").innerHTML = document.lastModified;
 
@@ -15,7 +15,7 @@ document.getElementById("modify").innerHTML = document.lastModified;
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const currentDate = new Date();
-let day = days[today.getDay()] + ", " + (today.getDate()) + " " + (months[today.getMonth()]) + " " + (today.getFullYear());
+let day = days[todayy.getDay()] + ", " + (todayy.getDate()) + " " + (months[todayy.getMonth()]) + " " + (todayy.getFullYear());
 document.querySelector("#date").textContent = day;
 
 

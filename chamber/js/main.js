251887365bd690484bb621a47d20c91b592e8dc1
sofiagalkina <gal18024@byui.edu>
2022-11-29 04:34:25 -0700

    hamburger button added

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -19,3 +19,14 @@ let day = days[today.getDay()] + ", " + (today.getDate()) + " " + (months[today.
 document.querySelector("#date").textContent = day;
 
 
+
+/* Hamburger Menu  */
+
+function myFunction() {
+    var x = document.getElementById("navbar");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  };

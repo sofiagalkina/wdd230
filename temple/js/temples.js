8f60698abc4018a2028112ba50bd5d4079edc8a9
sofiagalkina <gal18024@byui.edu>
2022-12-14 18:51:44 -0700

    hamburger menu fixed

--- temple/js/temples.js
+++ temple/js/temples.js
@@ -9,8 +9,13 @@ fetch(requestURL)
     
     const temple = jsonObject["temples"]
     temple.forEach(displayCards);
+   
 
+  })
+  .then( ()=>{
+    likeslist.forEach(displayLike);
   });
+  
 
   /*function displayCards(card){
     let cardview = document.querySelector("#cardview");
@@ -23,28 +28,59 @@ fetch(requestURL)
 
 */
 
-function displayCards(card){
-    let cardview = document.querySelector('#cardview');
-    let cardelt = document.createElement("div");
-    cardelt.innerHTML = `<img src="${card.imagesURL}"> 
-    <h2>${card.name} <span id='heart'>&#9825;</span></h2>
-    <p>${card.history} </p>
-    <p><strong>Address:</strong> ${card.address}</p>
-    <p><strong>Phone Number:</strong> ${card.telephone}</p>
-    <p><strong>Email:</strong> ${card.email}</p>
-    <p><strong>Services:</strong> ${card.services}</p>
 
-    <ul><li><strong>Ordinance Schedule:</strong> ${card.ordinance_schedule}</li>
-    <li><strong>Session Schedule:</strong> ${card.session_schedule}</li>
-    <li><strong>Closure Schedule:</strong>  ${card.closure_schedule}</li></ul>`;
-    cardview.appendChild(cardelt);
+
+
+
+// check to see whether there's already anything in the storage in the first place 
+const LIKES_KEY = "temple-likes";
+let likes_string = localStorage.getItem(LIKES_KEY);
+if (likes_string==null){
+  likes_string="[]";
+  localStorage.setItem(LIKES_KEY, likes_string);
+}
+
+
+// turning string to an array
+let likeslist = JSON.parse(likes_string);
+
+// displaying temple cards 
+function displayCards(card){
+  let cardview = document.querySelector('#cardview');
+  let cardelt = document.createElement("div");
+  cardelt.innerHTML = `<img src="${card.imagesURL}"> 
+  <span>${card.name}</span> <input class="mycheck" id="check-${card.id} type="checkbox" onclick="likeTemple(this);"></input>
+  <p>${card.history} </p>
+  <p><strong>Address:</strong> ${card.address}</p>
+  <p><strong>Phone Number:</strong> ${card.telephone}</p>
+  <p><strong>Email:</strong> ${card.email}</p>
+  <p><strong>Services:</strong> ${card.services}</p>
+
+  <ul><li><strong>Ordinance Schedule:</strong> ${card.ordinance_schedule}</li>
+  <li><strong>Session Schedule:</strong> ${card.session_schedule}</li>
+  <li><strong>Closure Schedule:</strong>  ${card.closure_schedule}</li></ul>`;
+  cardview.appendChild(cardelt);
+}
+
+function likeTemple(item){
+  let likes_string = localStorage.getItem(LIKES_KEY);
+  let likeslist = JSON.parse(likes_string);
+  if (item.checked){
+    if (!likeslist.includes(item.id)){
+      likeslist.push(item.id);
+    }
+  }
+  else{
+    if (likeslist.includes(item.id)){
+      likeslist.splice(likeslist.indexOf(item.id), 1);
+    }
+  }
+  localStorage.setItem(LIKES_KEY, JSON.stringify(likeslist));
 }
 
 
 
-const like = document.getElementById('heart');
-
-like.addEventListener('click', function onClick() {
-    like.style.backgroundColor = 'pink';
-    like.style.color = 'white';
-});
+function displayLike(item){
+  let obj = document.getElementById(item);
+  obj.checked = true;
+}
